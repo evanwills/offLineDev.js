caf1d7740de14303483b1adb1c0f9150f005e180
Evan Wills <evan.wills@acu.edu.au>
2015-05-04 10:53:05 +1000

    updated offlineDev.js; built out more of the functionality and started adding comments.

--- offlineDev.js
+++ offlineDev.js
@@ -1,6 +1,6 @@
-var reqquest = require('request');
+var request = require('request');
 
-module.exports = function (fileName, filePath, cssDevPath, jsDevPath) {
+module.exports = function (fileName, cssDevPath, jsDevPath, useMatrixFileNames) {
 	// check that file exists
 	// open file
 	// scrape file for <script src="">, <link rel="stylesheet" href="" />, <img> & <style>... url()
@@ -12,23 +12,29 @@ module.exports = function (fileName, filePath, cssDevPath, jsDevPath) {
 	// for off-line (non-dev) css check for image & font URLs and download those if they're not already cached and rewrite URLs
 	// for images, download and rewrite URLs.
 	var jsRegex = new RegExp('<script.*?src="([^"]+)".*?>', 'gi'),
-		linkRegex = new RegExp('<link\s.*?(?:rel="stylesheet".*?href="([^"]+)"|href="([^"]+)".*?rel="stylesheet").*?>', 'gi'),
+		linkedCSSregex = new RegExp('<link\s.*?(?:rel="stylesheet".*?href="([^"]+)"|href="([^"]+)".*?rel="stylesheet").*?>', 'gi'),
 		imgRegex = new RegExp('<img.*?src="([^"]+)".*?>', 'gi'),
-		styleRegex = new RegExp('<style[^>]*>(.*?)</style>', 'gis'),
-		fileContents,
-		rewriteDev,
-		cacheAssets,
+		styleRegex = new RegExp('<style[^>]*>\s*(.*?)\s*</style>', 'gis'),
+		styleURLRegex = new RegExp('url\(\s*[\'"]?(https?://[^\'")]+)[\'"]?\s*\)', 'gis'),
+		getFilePath,
+		getFileNameFunc,
+		getMatrixFileName,
+		getNormalFileName,
+		getFileExt,
+		fileContents = '',
+		cacheLocal,
 		isLocalDevJS,
 		isLocalDevCSS,
-		saveToCache;
+		rewritePath,
+		localRootPath;
 
-	function getDevFiles( path, extension ) {
+	function getDevFiles(path, extension) {
 		var tmpList = [],
-			output = [],
+			fileList = [],
 			i = 0;
 
 		if (!extension.match(/^[a-z]{2,4}$/i)) {
-			console.error('extension ("' + extionsion + '") is not a valid file extension.' );
+			console.error('extension ("' + extension + '") is not a valid file extension.');
 		}
 
 		try {
@@ -36,23 +42,140 @@ module.exports = function (fileName, filePath, cssDevPath, jsDevPath) {
 		} catch (e) {
 			console.log(path + ' is not a folder/directory');
 		}
-		for ( i = 0; i < tmpList.length; i + 1) {
+		for (i = 0; i < tmpList.length; i + 1) {
 			if (tmpList[i].match(/\.extension$/i)) {
 				fileList.push(fileList[i]);
 			}
 		}
-		return function(URL) {
-			fileName = URL.replace(/^.*\/([^?]+)(?:\?.*)$/,'$1');
-			if (fileList.inArray(fileName)) {
+		return function (URL) {
+			if (fileList.inArray(getFileNameFunc(URL))) {
 				return true;
 			} else {
 				return false;
 			}
-		}
+		};
 	}
 
-	function buildIsLocal( )
+	cacheLocal = function (URL, localPath) {
+		if (!fs.existsSync(localPath)) {
+			request(URL).pipe(fs.fs.writeFileSync(localPath));
+		}
+	};
 
-	this.isLocalDevCSS = getDevFiles(cssDevPath, 'css');
-	this.isLocalDevJS = getDevFiles(jsDevPath, 'js');
+
+	getNormalFileName = function (URL) {
+		var regex = new RegExp('^.*\/([^?#]+)(?:[\?#].*)$', 'i');
+		return URL.replace(regex, '$1');
+	};
+
+	getMatrixFileName = function (URL) {
+		var regex = new RegExp('^(.*\/)__data/assets/[a-z_]+/[0-9]+/([0-9]+)/([^?#]+)(?:[\?#].*)$', 'i');
+		return URL.replace(regex, '$1_$2');
+	};
+
+	getFileExt = function (tmpFileName) {
+		var regex = new RegExp('\.([a-z]{2,4})$', 'i');
+		return tmpFileName.replace(regex, '$1');
+	};
+
+	if (useMatrixFileNames === true) {
+		getFileNameFunc = getMatrixFileName;
+	} else {
+		getFileNameFunc = getNormalFileName;
+	}
+
+
+	rewritePath = function (URL) {
+		var tmpFileName = '',
+			localPath = '',
+			fileExt = '';
+
+		tmpFileName = getFileNameFunc(URL);
+		fileExt = getFileExt(tmpFileName);
+
+		if (fileExt === 'css') {
+			if (isLocalDevCSS(tmpFileName)) {
+				return cssDevPath + tmpFileName;
+			} else {
+				return 'css/' + tmpFileName;
+			}
+		}
+
+		if (fileExt === 'js') {
+			if (isLocalDevJS(tmpFileName)) {
+				return jsDevPath + tmpFileName;
+			} else {
+				localPath = 'css/' + tmpFileName;
+			}
+		} else if (fileExt.match('/png|jpe?g|gif|svg|ico/i')) {
+			localPath = 'images/' + tmpFileName;
+		} else if (fileExt.match('/woff2?|eot|ttf/i')) {
+			localPath = 'css/fonts/' + tmpFileName;
+		} else {
+			return URL;
+		}
+
+		cacheLocal(URL, localPath);
+		return localPath;
+	};
+
+
+	function rewriteCSSpath(CSScode) {
+		CSScode.replace(styleURLRegex,rewritePath());
+	}
+
+	isLocalDevCSS = getDevFiles(cssDevPath, 'css');
+	isLocalDevJS = getDevFiles(jsDevPath, 'js');
+
+	localRootPath = fileName.replace(/^(.*\/)[^\/]*$/, '$1');
+
+	if (!fs.existsSync(fileName)) {
+		console.error('could not open ' + fileName);
+		return false;
+	}
+
+// ==================================================================
+// check if cache directories exist on local file system
+
+	if (!fs.existsSync(localRootPath + 'images')) {
+		fs.mkdirSync(localRootPath + 'images');
+	}
+	if (!fs.existsSync(localRootPath + 'js')) {
+		fs.mkdirSync(localRootPath + 'js');
+	}
+	if (!fs.existsSync(localRootPath + 'css')) {
+		fs.mkdirSync(localRootPath + 'css');
+	}
+	if (!fs.existsSync(localRootPath + 'css/images')) {
+		fs.mkdirSync(localRootPath + 'css/images');
+	}
+	if (!fs.existsSync(localRootPath + 'css/fonts')) {
+		fs.mkdirSync(localRootPath + 'css/fonts');
+	}
+
+
+
+// ==================================================================
+// open HTML file for path rewriting (and caching of linked assets)
+	fileContents = fs.openSync(fileName, 'r+');
+
+// ------------------------------------------------------------------
+// incrementally save file for more immediate testing
+
+	fileContents.replace(linkedCSSregex, rewritePath()); // '$1$2'));
+	// save changes to the paths of linked CSS files
+	fs.writeFileSync(fileName, fileContents);
+
+	fileContents.replace(jsRegex, rewritePath()); // '$1'));
+	// save changes to the paths of linked JS files
+	fs.writeFileSync(fileName, fileContents);
+
+	fileContents.replace(imgRegex, rewritePath()); // '$1'));
+	// save changes to the paths of linked image files
+	fs.writeFileSync(fileName, fileContents);
+
+	fileContents.replace(styleRegex, rewriteCSSpath()); // '$1'));
+	// save changes to the paths of files linked CSS encapsulated
+	// within <STYLE> tags
+	fs.writeFileSync(fileName, fileContents);
 };
